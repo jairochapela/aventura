08042b5ffb51a6bb61494de9f7aa1513b00ebe1f
Jairo Chapela-Martínez <jairo@Mac-mini-de-Jairo.local>
2024-07-12 18:37:52 +0200

    Assets

--- mapa.py
+++ mapa.py
@@ -3,8 +3,8 @@ from room import Room
 estancias = {
     'entrada': Room("Estás en la entrada de la cueva. A tu alrededor ves oscuridad y humedad. A lo lejos, escuchas el sonido del agua.", ["linterna", "espada"], 'entrada'),
     'cueva': Room("Estás en una cueva oscura. El aire es húmedo y frío. Ves una luz al final del túnel.", ["antorcha", "llave"], 'cueva'),
-    'casa': Room("Estás en una casa, ves una mesa y una silla.", ["ipad"]),
-    'patio': Room("Estás en un patio, ves la casa, con su jardín y una piscina.", ["pala"]),
+    'casa': Room("Estás en una casa, ves una mesa y una silla.", ["ipad"], 'casa'),
+    'patio': Room("Estás en un patio, ves la casa, con su jardín y una piscina.", ["pala"], 'patio'),
     'playa': Room("Estás en una playa soleada, ves el mar y la arena. Las olas golpean las rocas. Ves algunas barcas en la orilla.", ["toalla"], 'playa'),
 }
 
